17ab4fd3d367ab65957718398a5a824fc6fac3ee
sveloo <sanny@edu360.com.au>
2020-04-04 22:21:40 +1100

    Completed Practice 5

--- 7-props/starter/src/Practice5.js
+++ 7-props/starter/src/Practice5.js
@@ -8,14 +8,14 @@ const Practice5 = () => {
     lastName: "Gordon",
     preferredName: "Zac",
     url: "https://zacgordon.com",
-    twitter: "zgordon"
+    twitter: "zgordon",
   };
   return (
     <div className="practice">
       {/*         
         1. Spread the "user" object into User so each "user" property becomes it's own prop
       */}
-      <User />
+      <User {...user} />
     </div>
   );
 };
@@ -26,12 +26,12 @@ const Practice5 = () => {
   4. Pass the username to <Username />
   5. Pass the url and twitter to <Social />
 */
-const User = () => {
+const User = ({ firstName, lastName, username, url, twitter }) => {
   return (
     <div className="user">
-      <FullName />
-      <Username />
-      <Social />
+      <FullName firstName={firstName} lastName={lastName} />
+      <Username username={username} />
+      <Social url={url} twitter={twitter} />
     </div>
   );
 };
@@ -39,7 +39,7 @@ const User = () => {
 /*
   6. Destructure the props needed
 */
-const FullName = props => (
+const FullName = ({ firstName, lastName }) => (
   <h1>
     {firstName} {lastName}
   </h1>
@@ -49,18 +49,20 @@ const FullName = props => (
   7. Create a <Username /> component that displays the username
 */
 
+const Username = ({ username }) => <p>{username}</p>;
+
 /*
   8. Destructure the props you will need
   9. Make the Website and Twitter links work based on props
 */
-const Social = props => {
+const Social = ({ twitter, url }) => {
   return (
     <ul className="social">
       <li>
-        <a>Website</a>
+        <a href={url}>Website</a>
       </li>
       <li>
-        <a>Twitter</a>
+        <a href={`https://www.twitter.com/${twitter}`}>{twitter}</a>
       </li>
     </ul>
   );
