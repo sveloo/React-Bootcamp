0bb5c53a098387b6f83cbb5a78ca62ee10ffd1fd
sveloo <sanny@edu360.com.au>
2020-04-05 13:46:48 +1000

    Completed Practice 4

--- 8-state/starter/src/index.js
+++ 8-state/starter/src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from "react-dom";
 
 // import Practice1 from "./Practice1";
 // import Practice2 from "./Practice2";
-import Practice3 from "./Practice3";
-// import Practice4 from "./Practice4";
+// import Practice3 from "./Practice3";
+import Practice4 from "./Practice4";
 // import Practice5 from "./Practice5";
 
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<Practice3 />, document.getElementById("root"));
+ReactDOM.render(<Practice4 />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
