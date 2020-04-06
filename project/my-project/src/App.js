3978b0cbb1a4d3813c1862f8eb6896733673d384
sveloo <sanny@edu360.com.au>
2020-04-06 22:12:11 +1000

    Completed Step 3 - Form

--- project/my-project/src/App.js
+++ project/my-project/src/App.js
@@ -1,35 +1,32 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
 import Header from "./components/Header";
 import Posts from "./components/Posts";
 import Post from "./components/Post";
+import PostForm from "./components/PostForm";
+import NotFound from "./components/NotFound";
 
 import "./App.css";
-import NotFound from "./components/NotFound";
 
 class App extends Component {
   state = {
     posts: [
       {
-        id: 1,
-        title: "Hello React",
-        slug: "hello-react",
-        content: "Lorem.",
-      },
-      {
-        id: 2,
-        title: "Hello Project",
-        slug: "hello-project",
-        content: "Tothe.",
-      },
-      {
-        id: 3,
-        title: "Hello Blog",
-        slug: "hello-blog",
-        content: "Ipsum.",
+        title: "",
+        content: "",
+        saved: false,
       },
     ],
+  };
+
+  addNewPost = (post) => {
+    post.id = this.state.posts.length + 1;
+    post.slug = encodeURIComponent(
+      post.title.toLowerCase().split(" ").join("-")
+    );
+    this.setState({
+      posts: [...this.state.posts, post],
+    });
   };
 
   render() {
@@ -53,6 +50,11 @@ class App extends Component {
                 else return <NotFound />;
                 return <Post post={post} />;
               }}
+            />
+            <Route
+              exact
+              path="/new"
+              render={() => <PostForm addNewPost={this.addNewPost} />}
             />
             <Route component={NotFound} />
           </Switch>
