16b97514768554b6493d6124fc8cb56dd603e131
sveloo <sanny@edu360.com.au>
2020-04-25 01:14:26 +1000

    Completed Step 07

--- project/project/src/App.js
+++ project/project/src/App.js
@@ -5,6 +5,7 @@ import {
   Route,
   Redirect,
 } from "react-router-dom";
+import SimpleStorage from "react-simple-storage";
 import Header from "./components/Header";
 import Posts from "./components/Posts";
 import Post from "./components/Post";
@@ -65,6 +66,7 @@ class App extends Component {
     return (
       <Router>
         <div className="App">
+          <SimpleStorage parent={this} />
           <Header />
           {this.state.message && <Message type={this.state.message} />}
           <Switch>
